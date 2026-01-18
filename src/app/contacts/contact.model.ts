2833708aed10dc8abc850781090bd19668c07866
K-Marple <taeshinoberu@outlook.com>
2026-01-17 22:36:50 -0800

    contact list and detail

--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -1,12 +1,12 @@
 export class Contact {
-    public id: number;
+    public id: string;
     public name: string;
     public email: string;
-    public phone: number;
+    public phone: string;
     public imageURL: string;
     public group: string;
 
-    constructor(id:number, name:string, email:string, phone:number, imageURL:string, group:string) {
+    constructor(id:string, name:string, email:string, phone:string, imageURL:string, group:string) {
         this.id = id;
         this.name = name;
         this.email = email;
