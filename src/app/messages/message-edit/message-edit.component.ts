7763d2479547bddeadb349c56ea62b289a30f46c
K-Marple <taeshinoberu@outlook.com>
2026-01-30 13:35:16 -0800

    message list and edit

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css',
 })
 export class MessageEditComponent {
+  @ViewChild("subjectInput") subjectInputRef: ElementRef;
+  @ViewChild("msgTextInput") msgTextInputRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = "Marple";
+
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSendMessage() {
+    const msgSubject = this.subjectInputRef.nativeElement.value;
+    const msgMsgText = this.msgTextInputRef.nativeElement.value;
+    const currentSender = this.currentSender;
+    const newMessage = new Message('7', msgSubject, msgMsgText, currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = "";
+    this.msgTextInputRef.nativeElement.value = "";
+  }
 
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css',
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message('3', 'Hello', 'This is an automated greeting.', 'Your computer'),
+    new Message('1', 'Letter', 'We just got a letter.', 'Blue'),
+    new Message('5', 'Hello Darkness', 'Hey old friend. Come to talk again.', 'Sleep'),
+    new Message('4', 'Song', 'Do you hear the song of angry men?', 'Drums')
+  ]
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 
 }
