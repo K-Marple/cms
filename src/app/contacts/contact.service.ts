5c6cfa5a999aa63c904866c796b660f5216efa87
K-Marple <taeshinoberu@outlook.com>
2026-02-18 20:17:26 -0800

    subjects and subscriptions for observables

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
 
@@ -7,7 +8,7 @@ import { MOCKCONTACTS } from './MOCKCONTACTS';
 })
 export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact>();
-  contactChangedEvent = new EventEmitter<Contact[]>();
+  contactChangedEvent = new Subject<Contact[]>();
 
   private contacts: Contact[] = []
 
@@ -41,6 +42,6 @@ export class ContactService {
       return;
     }
     this.contacts.splice(pos, 1);
-    this.contactChangedEvent.emit(this.contacts.slice());
+    this.contactChangedEvent.next(this.contacts.slice());
   }
 }
