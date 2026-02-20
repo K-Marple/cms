e93fb6ef5da8fb006bc5a9c4f68530f95ad11d5a
K-Marple <taeshinoberu@outlook.com>
2026-02-19 21:04:35 -0800

    add, update, delete documents and contacts

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -11,9 +11,11 @@ export class ContactService {
   contactChangedEvent = new Subject<Contact[]>();
 
   private contacts: Contact[] = []
+  private maxContactId: number;
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
 
   getContacts(): Contact[] {
@@ -42,6 +44,40 @@ export class ContactService {
       return;
     }
     this.contacts.splice(pos, 1);
+    this.contactChangedEvent.next(this.contacts.slice());
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    for (let contact of this.contacts){
+      const currentId = parseInt(contact.id, 10);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addContact(newContact: Contact) {
+    if (!newContact) {
+      return;
+    }
+    this.maxContactId++
+    newContact.id = this.maxContactId.toString();
+    this.contacts.push(newContact);
+    this.contactChangedEvent.next(this.contacts.slice());
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(originalContact);
+    if (pos < 0) {
+      return;
+    }
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
     this.contactChangedEvent.next(this.contacts.slice());
   }
 }
--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -11,9 +11,11 @@ export class DocumentService {
   documentChangedEvent = new Subject<Document[]>();
 
   private documents: Document[] = [];
+  private maxDocumentId: number;
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
   }
 
   getDocuments(): Document[] {
@@ -43,5 +45,41 @@ export class DocumentService {
     }
     this.documents.splice(pos, 1);
     this.documentChangedEvent.next(this.documents.slice());
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    for (let document of this.documents) {
+      const currentId = parseInt(document.id, 10);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addDocument(newDocument: Document) {
+    if (!newDocument) {
+      return;
+    }
+    this.maxDocumentId++
+    newDocument.id = this.maxDocumentId.toString();
+    this.documents.push(newDocument);
+    const documentsListClone = this.documents.slice()
+    this.documentChangedEvent.next(documentsListClone)
+  }
+
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    if (!originalDocument || !newDocument) {
+      return;
+    }
+    const pos = this.documents.indexOf(originalDocument)
+    if (pos < 0) {
+      return;
+    }
+    newDocument.id = originalDocument.id;
+    this.documents[pos] = newDocument;
+    const documentsListClone = this.documents.slice();
+    this.documentChangedEvent.next(documentsListClone);
   }
 }
