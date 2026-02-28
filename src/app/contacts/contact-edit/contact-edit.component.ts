d01721800fe45e167ce9e46600a1c06e281304de
K-Marple <taeshinoberu@outlook.com>
2026-02-28 13:03:28 -0800

    contact-edit form

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -8,17 +11,57 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrl: './contact-edit.component.css',
 })
 export class ContactEditComponent {
-  id: number;
-  editMode = false;
+  id: string;
+  originalContact: Contact;
+  contact: Contact;
+  editMode: boolean = false;
+  groupContacts: Contact[] = [];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private contactService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = +params["id"];
-        this.editMode = params["id"] != null;
+        this.id = params["id"];
+        if (!this.id) {
+          this.editMode = false;
+          return;
+        }
+        // this.editMode = params["id"] != null;
+        this.originalContact = this.contactService.getContact(this.id);
+        if (!this.originalContact) {
+          return;
+        }
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+        if (this.originalContact.group) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+        }
       }
     )
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newContact = new Contact(
+      value.id,
+      value.name,
+      value.email,
+      value.phone,
+      value.imageUrl,
+      value.group
+    );
+    if (this.editMode == true) {
+      this.contactService.updateContact(this.originalContact, newContact)
+    } else {
+      this.contactService.addContact(newContact)
+    }
+    this.router.navigate(["/contacts"]);
+  }
+
+  onCancel() {
+    this.router.navigate(["/contacts"]);
   }
 }
